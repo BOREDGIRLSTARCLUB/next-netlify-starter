9f00267cac1fb773d26e8bcb4191e4c57eabd47f
BOREDGIRLSTARCLUB <104789824+BOREDGIRLSTARCLUB@users.noreply.github.com>
2022-08-29 03:14:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,19 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Lonely ApePunks</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
+    <iframe
+src="https://gateway.ipfscdn.io/ipfs/Qma5FgmS9s3hkQrURfG4XDnh5Dxp9Fx9ZjBYkPFUNdQwfi/signature-drop.html?contract=0x9BC1557c0B4eF54b3070dD1465f4CA4A9B8373aB&chainId=137"
+width="600px"
+height="600px"
+style="max-width:100%;"
+frameborder="0"
+></iframe>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
