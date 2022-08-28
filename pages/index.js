00f3210408bb711612bd0a55be9d2f61b696e4aa
BOREDGIRLSTARCLUB <104789824+BOREDGIRLSTARCLUB@users.noreply.github.com>
2022-08-29 03:16:57 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,11 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
+    
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
     <iframe
 src="https://gateway.ipfscdn.io/ipfs/Qma5FgmS9s3hkQrURfG4XDnh5Dxp9Fx9ZjBYkPFUNdQwfi/signature-drop.html?contract=0x9BC1557c0B4eF54b3070dD1465f4CA4A9B8373aB&chainId=137"
 width="600px"
@@ -19,10 +24,6 @@ height="600px"
 style="max-width:100%;"
 frameborder="0"
 ></iframe>
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
 
       <Footer />
     </div>
